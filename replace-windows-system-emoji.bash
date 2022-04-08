ecf3610ffa14b451b2f5d7cb164e224f1ac3d979
z8kh8E6t rEv62qT7 <23183218+z8kh8E6t-rEv62qT7@users.noreply.github.com>
2022-04-08 19:30:53 +0800

    Update replace-windows-system-emoji.bash

--- replace-windows-system-emoji.bash
+++ replace-windows-system-emoji.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 SEGOE_UI_EMOJI=C:/Windows/Fonts/seguiemj.ttf
-SEGOE_UI_SYMBOL=C:/Windows/Fonts/seguisym.ttf
 TWEMOJI="Twemoji Mozilla.ttf"
 SEGOE_UI_EMOJI_MOD="Segoe UI Emoji with Twemoji.ttf"
-SEGOE_UI_SYMBOL_MOD="Segoe UI Symbol with Twemoji.ttf"
 
 if [ ./build/"$TWEMOJI" ]; then
     mv ./build/"$TWEMOJI" ./"$TWEMOJI"
@@ -15,8 +13,6 @@ if [ ! "$TWEMOJI" ]; then
     exit 1
 fi
 
-ttx -o "name.ttx" -t "name" "$SEGOE_UI_SYMBOL"
 ttx -o "emjname.ttx" -t "name" "$SEGOE_UI_EMOJI"
-ttx -o "$SEGOE_UI_SYMBOL_MOD" -m "$TWEMOJI" "name.ttx"
 ttx -o "$SEGOE_UI_EMOJI_MOD" -m "$TWEMOJI" "emjname.ttx"
-rm "emjname.ttx" "name.ttx"
+rm "emjname.ttx"
